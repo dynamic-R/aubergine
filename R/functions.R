92c51dc772a9470d1844ec56f1f03497f9c35610
tpetzoldt <thomas.petzoldt@tu-dresden.de>
2021-07-14 10:40:23 +0200

    outcomment exercise code

--- R/functions.R
+++ R/functions.R
@@ -31,76 +31,76 @@ AUBtutorial <- function(x = c(
       run_tutorial(Which, package = "aubergine")
   }
 }
-
-openRmdFile <- function(file, type) {
-
-  if (type == "RMD")
-     browseURL(file)
-  else if (type == "PDF")
-     browseURL(rmarkdown::render(input = file, output_format = "pdf_document",
-                       output_file=tempfile(fileext = ".pdf")))
-  else if (type == "HTML")
-     browseURL(rmarkdown::render(input = file, output_format = "html_document",
-                       output_file=tempfile(fileext = ".html")))
-  else if (type == "WORD")
-     browseURL(rmarkdown::render(input = file, output_format = "word_document",
-                       output_file=tempfile(fileext = ".doc")))
-}
-
-
-AUBexercise <- function(x = c("npzd", "riverAnoxia"),
-    type = c("HTML", "PDF", "RMD", "WORD")) {
-
-  LL <- as.character(formals(AUBexercise)$x[-1])
-  type <- match.arg(toupper(type), choices=c("HTML", "PDF", "RMD", "WORD"))
-
-  if (x == "?") {
-    exercise <- data.frame(x=LL, description = c(
-      "NPZD model (marine ecosystem model)",
-      "Anoxia in a river (1-D reaction transport model)"))
-    return(exercise)
-  } else {
-   if (is.character(x))
-     Which <- LL[pmatch(tolower(x), tolower(LL))]
-   else
-     Which <- LL[x]
-   Which <- paste(Which, "/", Which,"_Q", sep="")
-
-  # The files are stored in RMD format
-   RMD <- paste0(system.file('exercises', package = 'aubergine'),"/",Which, ".Rmd", sep="")
-
-   if (length(RMD) > 1) {
-     for (file in RMD) openRmdFile(file, type)
-   } else openRmdFile(RMD, type)
-  }
-}
-
-# A private function - to be used as aubergine:::AUBanswer
-AUBanswer <- function(x = c("npzd", "riverAnoxia"),
-    type = c("HTML", "PDF", "RMD", "WORD")) {
-
-  LL <- as.character(formals(AUBexercise)$x[-1])
-  type <- match.arg(toupper(type), choices=c("HTML", "PDF", "RMD", "WORD"))
-
-  if (x == "?") {
-    exercise <- data.frame(x=LL, description = c(
-      "NPZD model (marine ecosystem model)",
-      "Anoxia in a river (1-D reaction transport model)"
-      ))
-    return(exercise)
-  } else {
-   if (is.character(x))
-     Which <- LL[pmatch(tolower(x), tolower(LL))]
-   else
-     Which <- LL[x]
-   Which <- paste(Which, "/", Which, sep="")  # THIS DIFFERS FROM AUBexercise
-
-  # The files are stored in RMD format
-   RMD <- paste0(system.file('exercises', package = 'aubergine'),"/",Which, ".Rmd", sep="")
-
-   if (length(RMD) > 1) {
-     for (file in RMD) openRmdFile(file, type)
-   } else openRmdFile(RMD, type)
-  }
-}
-
+#
+# openRmdFile <- function(file, type) {
+#
+#   if (type == "RMD")
+#      browseURL(file)
+#   else if (type == "PDF")
+#      browseURL(rmarkdown::render(input = file, output_format = "pdf_document",
+#                        output_file=tempfile(fileext = ".pdf")))
+#   else if (type == "HTML")
+#      browseURL(rmarkdown::render(input = file, output_format = "html_document",
+#                        output_file=tempfile(fileext = ".html")))
+#   else if (type == "WORD")
+#      browseURL(rmarkdown::render(input = file, output_format = "word_document",
+#                        output_file=tempfile(fileext = ".doc")))
+# }
+#
+#
+# AUBexercise <- function(x = c("npzd", "riverAnoxia"),
+#     type = c("HTML", "PDF", "RMD", "WORD")) {
+#
+#   LL <- as.character(formals(AUBexercise)$x[-1])
+#   type <- match.arg(toupper(type), choices=c("HTML", "PDF", "RMD", "WORD"))
+#
+#   if (x == "?") {
+#     exercise <- data.frame(x=LL, description = c(
+#       "NPZD model (marine ecosystem model)",
+#       "Anoxia in a river (1-D reaction transport model)"))
+#     return(exercise)
+#   } else {
+#    if (is.character(x))
+#      Which <- LL[pmatch(tolower(x), tolower(LL))]
+#    else
+#      Which <- LL[x]
+#    Which <- paste(Which, "/", Which,"_Q", sep="")
+#
+#   # The files are stored in RMD format
+#    RMD <- paste0(system.file('exercises', package = 'aubergine'),"/",Which, ".Rmd", sep="")
+#
+#    if (length(RMD) > 1) {
+#      for (file in RMD) openRmdFile(file, type)
+#    } else openRmdFile(RMD, type)
+#   }
+# }
+#
+# # A private function - to be used as aubergine:::AUBanswer
+# AUBanswer <- function(x = c("npzd", "riverAnoxia"),
+#     type = c("HTML", "PDF", "RMD", "WORD")) {
+#
+#   LL <- as.character(formals(AUBexercise)$x[-1])
+#   type <- match.arg(toupper(type), choices=c("HTML", "PDF", "RMD", "WORD"))
+#
+#   if (x == "?") {
+#     exercise <- data.frame(x=LL, description = c(
+#       "NPZD model (marine ecosystem model)",
+#       "Anoxia in a river (1-D reaction transport model)"
+#       ))
+#     return(exercise)
+#   } else {
+#    if (is.character(x))
+#      Which <- LL[pmatch(tolower(x), tolower(LL))]
+#    else
+#      Which <- LL[x]
+#    Which <- paste(Which, "/", Which, sep="")  # THIS DIFFERS FROM AUBexercise
+#
+#   # The files are stored in RMD format
+#    RMD <- paste0(system.file('exercises', package = 'aubergine'),"/",Which, ".Rmd", sep="")
+#
+#    if (length(RMD) > 1) {
+#      for (file in RMD) openRmdFile(file, type)
+#    } else openRmdFile(RMD, type)
+#   }
+# }
+#
